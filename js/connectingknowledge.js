b03930de9550d16f611ead0d84c48c5865855662
joehalloran <joestandard@Mac-Pro-Joe.local>
2016-03-31 08:24:08 +0100

    Revert "time-line"
    This reverts commit 0479c738064f9a8d774953be3d2aef97b187b6f5.

--- js/connectingknowledge.js
+++ js/connectingknowledge.js
@@ -1,10 +1,20 @@
 $(function () {
+// 	var c=document.getElementById("myCanvas");
+// 	var ctx=c.getContext("2d");
+// 	ctx.strokeStyle = "#fff";
+// 	ctx.beginPath();
+// 	ctx.moveTo(0,0);
+// 	ctx.lineTo(300,150);
+// 	ctx.stroke();
+// 	var delay=1000; //1 seconds
 
-	// var $mySVG = $('svg').drawsvg();
-	// console.log($mySVG);
-	// $mySVG.drawsvg('animate');
+// setTimeout(function(){
+//   ctx.beginPath();
+// 	ctx.arc(95,50,40,0,2*Math.PI);
+// 	ctx.stroke();
+// }, delay); 
+
+
 	
-	  new Vivus('my-svg', {duration: 2000});
-
 });
 
