ebd07b7893917ad4d5d80177aa00604086fec08f
joehalloran <joestandard@Mac-Pro-Joe.local>
2016-04-15 15:09:47 +0100

    lesson plans and final corrections

--- js/connectingknowledge.js
+++ js/connectingknowledge.js
@@ -1,5 +1,5 @@
 $(function () {
-	$(window).load(function() {
+	function resizeBannerBar() {
 		var $containerWidth = $('.container').css("width").replace(/[^-\d\.]/g, '');
 		var $documentWidth = $(document).width();
 		var $titleMargin = ($documentWidth - $containerWidth) /  2;
@@ -7,6 +7,12 @@ $(function () {
 		var $total = parseFloat($titleMargin) + parseFloat ($titleWidth) + 45;
 		$('#banner-bar').css("min-width", $total);
 		$('#banner-bar').css("width", $total);
+	}
+	$(window).load(function() {
+		resizeBannerBar();
+	});
+	$( window ).resize(function() {
+		resizeBannerBar();
 	});
 });
 
